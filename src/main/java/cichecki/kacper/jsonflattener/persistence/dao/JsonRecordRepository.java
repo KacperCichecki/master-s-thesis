5ef0c68d5970cc585602c396302cafc97126b91b
Kacper Cichecki <kacper.cichecki@bluesoft.com>
2020-09-20 01:53:50 +0200

    spring Data

--- src/main/java/cichecki/kacper/jsonflattener/persistence/dao/JsonRecordRepository.java
+++ src/main/java/cichecki/kacper/jsonflattener/persistence/dao/JsonRecordRepository.java
@@ -4,7 +4,6 @@ import cichecki.kacper.jsonflattener.persistence.model.JsonRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JsonRecordRepository extends JpaRepository<JsonRecord, Long> {
 
     @Override
